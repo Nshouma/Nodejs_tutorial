d4a475dec4e799c8c0049dc1dd074bf5d4308df3
Nshouma <shouma.noguchi@nsw.co.jp>
2023-06-27 15:01:54 +0900

    自作モジュールサンプル

---
+++ sample11/index.js
@@ -0,0 +1,12 @@
+var {echo, area} = require("./methods");
+var Lamborghini = require("./lamborghini");
+var config = require("./config");
+
+echo("Hello World!");
+console.log(area(5, 10));
+
+var car = new Lamborghini("lamborghini");
+car.echo();
+car.drive();
+
+console.log(JSON.stringify(config));
