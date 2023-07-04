b9a0eafa3e134fe93ef5035e145f8d2d6b1dd482
Nshouma <shouma.noguchi@nsw.co.jp>
2023-07-04 11:08:39 +0900

    ファイル出力を追加

--- sample19/index.js
+++ sample19/index.js
@@ -11,3 +11,12 @@ reader.on("end", () => {
   console.log(data);
 });
 reader.resume();
+
+//任意の文字列をテキストファイルに出力
+var writer = fs.createWriteStream(path.join(__dirname, "created.txt"), "utf8");
+writer.end("Hello world!");
+
+//読み込んだファイルをパイプして出力
+var write = fs.createWriteStream(path.join(__dirname, "sample-copy.txt"), "utf8");
+reader.pipe(write);
+reader.resume();
