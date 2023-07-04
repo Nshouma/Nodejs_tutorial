59cf9bb61276a7b5c8c5bad68b747127bc6ec91b
Nshouma <shouma.noguchi@nsw.co.jp>
2023-07-04 16:06:43 +0900

    customwriter.jsを追加

---
+++ sample20/customwriter.js
@@ -0,0 +1,27 @@
+//受け取ったストリームをコンソールに書き出す
+const {Writable} = require("stream");
+
+var CustomWriter = class extends Writable {
+  constructor(options){
+    super(options || {decodeString: true});
+  }
+
+  _write(chunk, encoding, done){
+    var text;
+    if(encoding === "buffer"){
+      text = chunk.toString(this._writableState.defaultEncoding);
+    } else {
+      text = chunk;
+    }
+
+    try{
+      console.log(text);
+    } catch(error){
+      done(error);
+      return;
+    }
+    done();
+  }
+};
+
+module.exports = CustomWriter;
--- sample20/index.js
+++ sample20/index.js
@@ -2,8 +2,15 @@
 //console.log(randomstring());
 
 var CustomReader = require("./customreader.js");
-var reader = new CustomReader();
+var CustomWriter = require("./customwriter.js");
+
+/* var reader = new CustomReader();
 reader.on("data", (chunk)=>{
   console.log(chunk);
 });
+reader.resume(); */
+
+var reader = new CustomReader();
+var writer = new CustomWriter();
+reader.pipe(writer);
 reader.resume();
