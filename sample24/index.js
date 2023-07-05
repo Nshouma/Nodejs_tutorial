2de1a2787fcf4b9173f1826553b4e500c0cf5d8b
Nshouma <shouma.noguchi@nsw.co.jp>
2023-07-05 11:58:51 +0900

    レスポンス情報

--- sample24/index.js
+++ sample24/index.js
@@ -1,10 +1,19 @@
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
 var server = http.createServer((req, res) => {
   console.log(
     `[${(new Date()).toISOString()}]`+
     `${req.method} ${req.url}`+
     `${req.headers["user-agent"]}`
   );
-  res.end("hello world!");
+  if(req.method==="POST"){
+    req.pipe(res);
+  }else{
+    var reader = fs.createReadStream(path.join(__dirname, "sample.txt"), "utf8");
+    reader.pipe(res);
+    //res.end("hello world!");
+  }
 });
 server.listen(3000);
